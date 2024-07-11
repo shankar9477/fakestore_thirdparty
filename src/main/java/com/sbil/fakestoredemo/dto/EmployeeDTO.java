9335d1419580d6c709c3b45b5671d5a4571a0888
shankar <shankaryabaji11@gmail.com>
2024-07-11 13:14:24 +0530

    second commit

--- src/main/java/com/sbil/fakestoredemo/dto/EmployeeDTO.java
+++ src/main/java/com/sbil/fakestoredemo/dto/EmployeeDTO.java
@@ -1,2 +1,9 @@
-package com.sbil.fakestoredemo.dto;public class EmployeeDTO {
+package com.sbil.fakestoredemo.dto;
+
+public class EmployeeDTO
+{
+    int id ;
+    String name ;
+    String address ;
+
 }
